428733350b1dce2104994a093cb2cef7ea2e97d0
lucas-sancere <lucas.sancere@curie.fr>
2021-10-13 12:17:42 +0200

    sc

--- examples/Scripts/DenoiseCutandSegment.py
+++ examples/Scripts/DenoiseCutandSegment.py
@@ -13,20 +13,22 @@ from tifffile import imread, imwrite
 from stardist.models import StarDist3D
 from csbdeep.models import Config, CARE
 from vollseg import SmartSeedPrediction3D
-os.environ["CUDA_VISIBLE_DEVICES"]="2"
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
 os.environ["HDF5_USE_FILE_LOCKING"] = "FALSE"
 from pathlib import Path
 from n2v.models import  N2V
 
-ImageDir = '/data/u934/service_imagerie/v_kapoor/Twitter/Test/'
-Model_Dir = '/data/u934/service_imagerie/v_kapoor/Twitter/'
+ImageDir = '/home/sancere/Kepler/ClaudiaData/148D_each10min/Greens/SplitTimelapse/'
+Model_Dir = '/home/sancere/Kepler/CurieDeepLearningModels/MouseClaudia/GreenCell3D/'
 
 SaveDir = ImageDir + 'Results/'
 
 
-NoiseModelName = 'ArabidopsisDenoising'
-UNETModelName = 'UNETArabidopsisVollSeg'
-StarModelName = 'ArabidopsisVollSeg'
+NoiseModelName = 'ScipyDenoising'
+UNETModelName = 'UNETScipyDeepGreenCells'
+StarModelName = 'ScipyDeeperGreenCells'
+
+
 UnetModel = CARE(config = None, name = UNETModelName, basedir = Model_Dir)
 StarModel = StarDist3D(config = None, name = StarModelName, basedir = Model_Dir)
 NoiseModel = N2V(config=None, name=NoiseModelName, basedir=Model_Dir)
@@ -35,28 +37,28 @@ Raw_path = os.path.join(ImageDir, '*.tif')
 filesRaw = glob.glob(Raw_path)
 filesRaw.sort
 min_size = 10
-n_tiles = (2,2,2)
+n_tiles = (2,8,8)
 
 
-fname = filesRaw[0]
-test_image_dimensions = imread(fname)
-if len(test_image_dimensions) > 3:
+#fname = filesRaw[0]
+#test_image_dimensions = imread(fname)
+#if len(test_image_dimensions) > 3:
       #Split the image
-      SplitImageDir = ImageDir + 'SplitTimelapse/'
-      Path(SplitImageDir).mkdir(exist_ok=True)
-      for fname in filesRaw:
-         image = imread(fname)
-         Name = os.path.basename(os.path.splitext(fname)[0])
-         for i in range(image.shape[0]):
+      #SplitImageDir = ImageDir + 'SplitTimelapse/'
+      #Path(SplitImageDir).mkdir(exist_ok=True)
+      #for fname in filesRaw:
+         #image = imread(fname)
+         #Name = os.path.basename(os.path.splitext(fname)[0])
+         #for i in range(image.shape[0]):
 
-              split_image = image[i,:]
-              imwrite(SplitImageDir + Name + str(i) + '.tif' , split_image.astype('float16'))
-else:
+              #split_image = image[i,:]
+              #imwrite(SplitImageDir + Name + str(i) + '.tif' , split_image.astype('float16'))
+#else:
 
-       SplitImageDir = ImageDir  
+       #SplitImageDir = ImageDir  
 
 
-for fname in SplitImageDir:
+for fname in filesRaw:
      
-     SmartSeedPrediction3D(SaveDir, fname, UnetModel, StarModel,NoiseModel, min_size = min_size,  n_tiles = n_tiles, UseProbability = False)
+     SmartSeedPrediction3D(SaveDir, fname, UnetModel, StarModel,NoiseModel, min_size = min_size,  n_tiles = n_tiles, UseProbability = False, filtersize = 2)
 
--- examples/Scripts/DenoiseandSegment.py
+++ examples/Scripts/DenoiseandSegment.py
@@ -13,20 +13,20 @@ from tifffile import imread, imwrite
 from stardist.models import StarDist3D
 from csbdeep.models import Config, CARE
 from vollseg import SmartSeedPrediction3D
-os.environ["CUDA_VISIBLE_DEVICES"]="2"
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
 os.environ["HDF5_USE_FILE_LOCKING"] = "FALSE"
 from pathlib import Path
 from n2v.models import  N2V
 
-ImageDir = '/data/u934/service_imagerie/v_kapoor/Twitter/Test/'
-Model_Dir = '/data/u934/service_imagerie/v_kapoor/Twitter/'
+ImageDir = '/home/sancere/Kepler/ClaudiaData/148D_each10min/Greens/'
+Model_Dir = '/home/sancere/Kepler/CurieDeepLearningModels/MouseClaudia/GreenCell3D/'
 
 SaveDir = ImageDir + 'Results/'
 
 
-NoiseModelName = 'ArabidopsisDenoising'
-UNETModelName = 'UNETArabidopsisVollSeg'
-StarModelName = 'ArabidopsisVollSeg'
+NoiseModelName = 'ScipyDenoising'
+UNETModelName = 'UNETScipyDeepGreenCells'
+StarModelName = 'ScipyDeeperGreenCells'
 UnetModel = CARE(config = None, name = UNETModelName, basedir = Model_Dir)
 StarModel = StarDist3D(config = None, name = StarModelName, basedir = Model_Dir)
 NoiseModel = N2V(config=None, name=NoiseModelName, basedir=Model_Dir)
@@ -37,7 +37,9 @@ filesRaw.sort
 min_size = 10
 n_tiles = (2,2,2)
 
+
+
 for fname in filesRaw:
      
-     SmartSeedPrediction3D(SaveDir, fname, UnetModel, StarModel,NoiseModel, min_size = min_size,  n_tiles = n_tiles, UseProbability = False)
+     SmartSeedPrediction3D(SaveDir, fname, UnetModel, StarModel,NoiseModel, min_size = min_size,  n_tiles = n_tiles, UseProbability = False, filtersize = 2)
 
