bde155418862bfd4db41379f2586704adb2df594
kapoorlab <randomaccessiblekapoor@gmail.com>
2021-12-22 00:23:02 +0100

    zoos

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as f:
 setup(
     name="vollseg",
 
-    version='2.2.1',
+    version='2.2.3',
 
     author='Varun Kapoor,Claudia Carabana Garcia,Mari Tolonen',
     author_email='randomaccessiblekapoor@gmail.com',
--- vollseg/__init__.py
+++ vollseg/__init__.py
@@ -22,13 +22,24 @@ register_model(Unet2D,   'Unet_2D_cells', 'Unet some model zoo 2D', 'unetsomehas
 register_model(Unet3D,   'Unet_3D_cells',  'Unet somemodel zoo 3D', 'unetsomehash3d')
 register_model(CARE,   'Denoise_3D_cells',  'Unet somemodel zoo 3D', 'unetsomehash3d')              
 register_model(CARE,   'Denoise_2D_cells',  'Unet somemodel zoo 3D', 'unetsomehash3d') 
-
+register_model(StarDist2D,   '2D_cells_sec', 'some model zoo 2D', 'somehash')
+register_model(StarDist3D,   '3D_cells_sec',  'somemodel zoo 3D', 'somehash3d')
+register_model(Unet2D,   'Unet_2D_cells_sec', 'Unet some model zoo 2D sec', 'unetsomehashsec')
+register_model(Unet3D,   'Unet_3D_cells_sec',  'Unet somemodel zoo 3D sec', 'unetsomehash3dsec')
+register_model(CARE,   'Denoise_3D_cells_sec',  'Unet somemodel zoo 3D sec', 'unetsomehash3dsec')              
+register_model(CARE,   'Denoise_2D_cells_sec',  'Unet somemodel zoo 3D sec', 'unetsomehash3dsec') 
 
 register_aliases(StarDist2D, '2D_cells', 'Alias some model zoo 2D')
 register_aliases(StarDist3D, '3D_cells',   'Alias some model zoo 3D')
 register_aliases(Unet2D, 'Unet_2D_cells',  'Alias Unet some model zoo 2D')
 register_aliases(Unet3D, 'Unet_3D_cells',  'Alias Unet some model zoo 3D')
-register_aliases(CARE, 'Denoise_2D_cells',  'Alias Unet some model zoo 2D')
-register_aliases(CARE, 'Denoise_3D_cells',  'Alias Unet some model zoo 3D')
+register_aliases(CARE, 'Denoise_2D_cells',  'Alias Unet some model zoo 2D den')
+register_aliases(CARE, 'Denoise_3D_cells',  'Alias Unet some model zoo 3D den ')
+register_aliases(StarDist2D, '2D_cells_sec', 'Alias some model zoo 2D sec')
+register_aliases(StarDist3D, '3D_cells_sec',   'Alias some model zoo 3D sec')
+register_aliases(Unet2D, 'Unet_2D_cells_sec',  'Alias Unet some model zoo 2D sec')
+register_aliases(Unet3D, 'Unet_3D_cells_sec',  'Alias Unet some model zoo 3D sec')
+register_aliases(CARE, 'Denoise_2D_cells_sec',  'Alias Unet some model zoo 2D den sec')
+register_aliases(CARE, 'Denoise_3D_cells_sec',  'Alias Unet some model zoo 3D den sec')
 
 del register_model, register_aliases, clear_models_and_aliases
