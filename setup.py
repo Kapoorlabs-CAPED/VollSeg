57b82717fef8da4eb5f2f312430e08177be5f620
kapoorlab <varunkapoor@localhost.localdomain>
2021-12-23 16:30:29 +0100

    dev

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as f:
 setup(
     name="vollseg",
 
-    version='2.3.3',
+    version='2.3.4',
 
     author='Varun Kapoor,Claudia Carabana Garcia,Mari Tolonen',
     author_email='randomaccessiblekapoor@gmail.com',
