872d9f25ea908a715a5a933fde96717821dbf372
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-02-06 16:24:14 +0100

    star voll

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as f:
 setup(
     name="vollseg",
 
-    version='3.0.4',
+    version='3.0.5',
 
     author='Varun Kapoor,Claudia Carabana Garcia,Mari Tolonen',
     author_email='randomaccessiblekapoor@gmail.com',
