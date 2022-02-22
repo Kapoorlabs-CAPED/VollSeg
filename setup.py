7513dd9681da2ba3b31d2f21e88a4a97baec9bb2
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-02-22 15:56:23 +0100

    new version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as f:
 setup(
     name="vollseg",
 
-    version='4.0.0',
+    version='4.0.1',
 
     author='Varun Kapoor,Claudia Carabana Garcia,Mari Tolonen',
     author_email='randomaccessiblekapoor@gmail.com',
