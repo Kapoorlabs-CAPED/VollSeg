e7c377e4c7d9eedb38a3ebaface580f6bfb3bd13
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-03-15 10:48:32 +0100

    voll nap

--- vollseg/__init__.py
+++ vollseg/__init__.py
@@ -30,12 +30,12 @@ register_model(CARE,   'Denoise_carcinoma',  'https://zenodo.org/record/5910645/
 
 
 register_model(UNET,   'Embryo Cell Model (3D)',  'https://zenodo.org/record/6337699/files/embryo_cell_model.zip', 'c84fdec38a5b3cc6c1869c94ff23f3b')
-
+register_model(UNET,   'Xenopus Tissue (2D)',  'https://zenodo.org/record/6060378/files/Xenopus_tissue_model.zip', '2694d8b05fa828aceb055eef8cd5ca1f')
 register_model(StarDist2D,   'White_Blood_Cells',  'https://zenodo.org/record/5815521/files/WBCSeg.zip', '7889f5902d8562766a4dee2726c90d49')
 register_model(StarDist3D,   'Carcinoma_cells',  'https://zenodo.org/record/6354077/files/carcinoma_stardist.zip', 'b92b9d5347862e52279629be575fe0b7')
 
 register_model(UNET,   'Unet_White_Blood_Cells',  'https://zenodo.org/record/5815588/files/UNETWBC.zip', '9645f004db478f661811d6da615ccc0b')
-register_model(MASKUNET,   'Xenopus_Tissue_Segmentation',  'https://zenodo.org/record/6060378/files/Xenopus_tissue_model.zip', '2694d8b05fa828aceb055eef8cd5ca1f')
+register_model(MASKUNET,   'Xenopus_Cell_Tissue_Segmentation',  'https://zenodo.org/record/6060378/files/Xenopus_tissue_model.zip', '2694d8b05fa828aceb055eef8cd5ca1f')
 register_model(UNET,   'Unet_Cyto_White_Blood_Cells',  'https://zenodo.org/record/5815603/files/UNETcytoWBC.zip', 'dd3bf8b8e2a04536144954e882445a5e') 
 register_model(MASKUNET,   'Unet_Lung_Segmentation',  'https://zenodo.org/record/6060177/files/Montgomery_county.zip', 'be41937a00693e28961358440d242417') 
 
@@ -46,8 +46,9 @@ register_aliases(StarDist2D, 'White_Blood_Cells',   'White_Blood_Cells')
 register_aliases(StarDist3D, 'Carcinoma_cells',   'Carcinoma_cells')
 register_aliases(UNET, 'Unet_White_Blood_Cells',  'Unet_White_Blood_Cells')
 register_aliases(UNET, 'Unet_Cyto_White_Blood_Cells',  'Unet_Cyto_White_Blood_Cells')
+register_aliases(UNET, 'Xenopus Tissue (2D)',  'Xenopus Tissue (2D)')
 register_aliases(MASKUNET, 'Unet_Lung_Segmentation',  'Unet_Lung_Segmentation')
-register_aliases(MASKUNET, 'Xenopus_Tissue_Segmentation',  'Xenopus_Tissue_Segmentation')
+register_aliases(MASKUNET, 'Xenopus_Cell_Tissue_Segmentation',  'Xenopus_Cell_Tissue_Segmentation')
 
 register_aliases(CARE, 'Denoise_3D_cells',  'Denoise_3D_cells')
 
