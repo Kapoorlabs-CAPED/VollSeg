65218e33b826e68dbba2c2490819658c0dc53e9a
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-11-25 18:36:44 +0100

    VollSeg and CellPose get married

--- vollseg/utils.py
+++ vollseg/utils.py
@@ -926,8 +926,8 @@ def VollCellSeg(image: np.ndarray,
             
             if star_model is not None:
                 if prob_thresh is None and nms_thresh is None:
-                        prob_thresh = model.thresholds.prob
-                        nms_thresh = model.thresholds.nms
+                        prob_thresh = star_model.thresholds.prob
+                        nms_thresh = star_model.thresholds.nms
                 res = VollSeg3D(image_nuclei,  unet_model, star_model, roi_model=roi_model,ExpandLabels= ExpandLabels,  axes=axes, noise_model=noise_model, prob_thresh=prob_thresh, nms_thresh=nms_thresh, donormalize=donormalize, lower_perc=lower_perc, upper_perc=upper_perc, min_size_mask=min_size_mask, min_size=min_size, max_size=max_size,
                                     n_tiles=n_tiles, UseProbability=UseProbability,  dounet=dounet, seedpool=seedpool, startZ=startZ, slice_merge=slice_merge, iou_threshold=iou_threshold)
 
@@ -959,8 +959,8 @@ def VollCellSeg(image: np.ndarray,
             
             if star_model is not None:
                   if prob_thresh is None and nms_thresh is None:
-                        prob_thresh = model.thresholds.prob
-                        nms_thresh = model.thresholds.nms
+                        prob_thresh = star_model.thresholds.prob
+                        nms_thresh = star_model.thresholds.nms
                   res = tuple(
                      zip(
                         *tuple(VollSeg3D(_x,  unet_model, star_model, axes=axes, noise_model=noise_model, roi_model=roi_model,ExpandLabels= ExpandLabels,  prob_thresh=prob_thresh, nms_thresh=nms_thresh, donormalize=donormalize, lower_perc=lower_perc, upper_perc=upper_perc, min_size_mask=min_size_mask, min_size=min_size, max_size=max_size,
