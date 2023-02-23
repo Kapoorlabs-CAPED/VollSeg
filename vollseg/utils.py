85508ab642f77389c12d9d4fc93d675b28a4cef5
kapoorlab <randomaccessiblekapoor@gmail.com>
2023-02-23 14:46:10 +0100

    median size

--- vollseg/utils.py
+++ vollseg/utils.py
@@ -2529,7 +2529,7 @@ def CellPoseWater(cellpose_mask, sized_smart_seeds, cellpose_base, nms_thresh, z
     
     markers = morphology.dilation(
         markers_raw.astype('uint16'), morphology.ball(2))                
-    watershed_image_nuclei = watershed(-median_filter(cellpose_base, sigma = 1), markers, mask = probability_mask)
+    watershed_image_nuclei = watershed(-median_filter(cellpose_base, size = 2), markers, mask = probability_mask)
     watershed_image_nuclei = fill_label_holes(watershed_image_nuclei)
    
     cellpose_mask_copy = cellpose_mask.copy()
