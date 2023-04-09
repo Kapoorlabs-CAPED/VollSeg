3fafb0a2abbd98faff469f8876b684fac75f39c4
kapoorlab <randomaccessiblekapoor@gmail.com>
2023-04-10 00:47:26 +0200

    show time

--- src/vollseg/utils.py
+++ src/vollseg/utils.py
@@ -43,12 +43,11 @@ from skimage.util import invert as invertimage
 from tifffile import imread, imwrite
 from tqdm import tqdm
 from .CellPose3D import CellPose3DModel
-from .PredictTiledLoader import PredictTiled
 from vollseg.matching import matching
 from vollseg.nmslabel import NMSLabel
 from vollseg.seedpool import SeedPool
 from vollseg.unetstarmask import UnetStarMask
-from .Tiles_3D import VolumeMerger
+from .Tiles_3D import VolumeMerger, VolumeSlicer
 
 Boxname = "ImageIDBox"
 GLOBAL_THRESH = 1.0e-2
@@ -1443,16 +1442,19 @@ def _apply_cellpose_network_3D(
     model = model.load_from_checkpoint(cellpose_model_3D_pretrained_file)
     model.eval()
 
-    # predict_model = CellPose3DPredict(model, hparams)
+    merger = VolumeMerger(image_membrane.shape, out_channels)
 
-    dataset = PredictTiled(
-        image=image_membrane, patch_size=patch_size, patch_step=patch_step
+    tiler = VolumeSlicer(
+        image_membrane.shape,
+        voxel_size=patch_size,
+        voxel_step=patch_step,
     )
 
-    data_loader = DataLoader(dataset, batch_size=batch_size)
-    merger = VolumeMerger(image_membrane.shape, out_channels)
-    for data in data_loader:
-        tiles_batch, coords_batch = data
+    tiles = tiler.split(image_membrane)
+
+    for tiles_batch, coords_batch in DataLoader(
+        list(zip(tiles, tiler.crops)), batch_size=batch_size, pin_memory=True
+    ):
         try:
             pred_tile = model(tiles_batch.cuda())
         except ValueError:
