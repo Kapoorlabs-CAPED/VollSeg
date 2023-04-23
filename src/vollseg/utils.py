fcdb1e96a4a2e12960bc6af9f4ca202095a72621
kapoorlab <randomaccessiblekapoor@gmail.com>
2023-04-23 15:50:01 +0200

    update cellpose stitching

--- src/vollseg/utils.py
+++ src/vollseg/utils.py
@@ -5547,26 +5547,27 @@ def stitch3D(masks, stitch_threshold=0.25):
     empty = 0
 
     for i in range(len(masks) - 1):
-        iou = _intersection_over_union(masks[i + 1], masks[i])[1:, 1:]
-        if not iou.size and empty == 0:
-            masks[i + 1] = masks[i + 1]
-            mmax = masks[i + 1].max()
-        elif not iou.size and not empty == 0:
-            icount = masks[i + 1].max()
-            istitch = np.arange(mmax + 1, mmax + icount + 1, 1, int)
-            mmax += icount
-            istitch = np.append(np.array(0), istitch)
-            masks[i + 1] = istitch[masks[i + 1]]
-        else:
-            iou[iou < stitch_threshold] = 0.0
-            iou[iou < iou.max(axis=0)] = 0.0
-            istitch = iou.argmax(axis=1) + 1
-            ino = np.nonzero(iou.max(axis=1) == 0.0)[0]
-            istitch[ino] = np.arange(mmax + 1, mmax + len(ino) + 1, 1, int)
-            mmax += len(ino)
-            istitch = np.append(np.array(0), istitch)
-            masks[i + 1] = istitch[masks[i + 1]]
-            empty = 1
+        if masks[i].max() > 0 and masks[i + 1].max() > 0:
+            iou = _intersection_over_union(masks[i + 1], masks[i])[1:, 1:]
+            if not iou.size and empty == 0:
+                masks[i + 1] = masks[i + 1]
+                mmax = masks[i + 1].max()
+            elif not iou.size and not empty == 0:
+                icount = masks[i + 1].max()
+                istitch = np.arange(mmax + 1, mmax + icount + 1, 1, int)
+                mmax += icount
+                istitch = np.append(np.array(0), istitch)
+                masks[i + 1] = istitch[masks[i + 1]]
+            else:
+                iou[iou < stitch_threshold] = 0.0
+                iou[iou < iou.max(axis=0)] = 0.0
+                istitch = iou.argmax(axis=1) + 1
+                ino = np.nonzero(iou.max(axis=1) == 0.0)[0]
+                istitch[ino] = np.arange(mmax + 1, mmax + len(ino) + 1, 1, int)
+                mmax += len(ino)
+                istitch = np.append(np.array(0), istitch)
+                masks[i + 1] = istitch[masks[i + 1]]
+                empty = 1
 
     return masks
 
