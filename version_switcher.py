eaacdd1d65afab0b49c5b3041a9b779138549a78
kapoorlab <randomaccessiblekapoor@gmail.com>
2023-04-23 23:13:24 +0200

    star the real manual

---
+++ version_switcher.py
@@ -0,0 +1,12 @@
+[
+    {
+        "name": "dev",
+        "version": "29.9.0.dev0",
+        "url": "https://github.com/Kapoorlabs-CAPED/VollSeg/",
+    },
+    {
+        "name": "29.9.0 (stable)",
+        "version": "29.9.0",
+        "url": "https://github.com/Kapoorlabs-CAPED/VollSeg/releases/tag/v29.9.0/",
+    },
+]
