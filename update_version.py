f9bb0a7744a886f560ece34633be69b379865f94
kapoorlab <randomaccessiblekapoor@gmail.com>
2023-09-04 14:54:43 +0200

    add custom hook for versioning

---
+++ update_version.py
@@ -0,0 +1,18 @@
+import subprocess
+
+
+def update_version_file():
+    # Get the latest Git tag
+    tag = (
+        subprocess.check_output(["git", "describe", "--tags", "--abbrev=0"])
+        .decode()
+        .strip()
+    )
+
+    # Update _version.py
+    with open("_version.py", "w") as version_file:
+        version_file.write(f'__version__ = "{tag}"\n')
+
+
+if __name__ == "__main__":
+    update_version_file()
