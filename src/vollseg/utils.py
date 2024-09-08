869c94a8a3bb71e6465f4c95f594f4aefd8b3edc
kapoorlab <varun.kapoor@kapoorlabs.org>
2024-09-08 19:45:04 +0000

    model

--- src/vollseg/utils.py
+++ src/vollseg/utils.py
@@ -4581,9 +4581,8 @@ def simple_dist(label_image):
 
     # Create an empty output image
     binary_image = np.zeros_like(label_image, dtype=np.float32)
-    for i in range(binary_image.shape[0]):
-       binary_image[i] = find_boundaries(label_image[i], mode="outer") * 255
-       binary_image[i] = gaussian_filter(binary_image[i], sigma = 2)
+    binary_image[i] = find_boundaries(label_image, mode="outer") * 255
+    binary_image[i] = gaussian_filter(binary_image, sigma = 2)
     output_image = binary_image / np.max(binary_image)
     return output_image  
 
